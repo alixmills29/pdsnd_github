2e97ae969ef5e368b0166a750c712c64b62d43da
Alix Mills <alixmills@comcast.net>
2019-12-15 13:53:59 -0500

    Updated restart message for bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -207,7 +207,7 @@ def main():
         user_stats(df)
         see_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart the bikeshare statistics program? Please enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
